b673e17dbc78baa21a642f4e1a98f4448463e5c8
Minh2306 <95114707+MInh2306@users.noreply.github.com>
2022-06-06 21:10:37 +0900

    aa
    aa

---
+++ Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,67 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CarController::Start()
+extern void CarController_Start_m10BA7CB090F1049BB7AC3D4C4BCFA943380B7882 (void);
+// 0x00000002 System.Void CarController::Update()
+extern void CarController_Update_m69DDDE13273382E3C25AD2D77EC70DFFE3A70739 (void);
+// 0x00000003 System.Void CarController::.ctor()
+extern void CarController__ctor_m180A0B729C25B52327B05362FB3A7AA7A53A9739 (void);
+// 0x00000004 System.Void GameDirector::Start()
+extern void GameDirector_Start_m92DAF8BCF048D1F77B51F51BCD4CA47D260F6DC9 (void);
+// 0x00000005 System.Void GameDirector::Update()
+extern void GameDirector_Update_m619B224507888150A939D1E0917CA9F27BDB090D (void);
+// 0x00000006 System.Void GameDirector::.ctor()
+extern void GameDirector__ctor_m806FADCD19DEA27E4920565E5FF62352956A937C (void);
+static Il2CppMethodPointer s_methodPointers[6] = 
+{
+	CarController_Start_m10BA7CB090F1049BB7AC3D4C4BCFA943380B7882,
+	CarController_Update_m69DDDE13273382E3C25AD2D77EC70DFFE3A70739,
+	CarController__ctor_m180A0B729C25B52327B05362FB3A7AA7A53A9739,
+	GameDirector_Start_m92DAF8BCF048D1F77B51F51BCD4CA47D260F6DC9,
+	GameDirector_Update_m619B224507888150A939D1E0917CA9F27BDB090D,
+	GameDirector__ctor_m806FADCD19DEA27E4920565E5FF62352956A937C,
+};
+static const int32_t s_InvokerIndices[6] = 
+{
+	1117,
+	1117,
+	1117,
+	1117,
+	1117,
+	1117,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	6,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
